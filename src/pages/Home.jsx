ded9c6869f25f652b48675518e53b459dbff29bb
Cherchel <tvink_tvink2015_tvink@mail.ru>
2023-02-06 03:24:25 +1000

    Сохранение параметров фильтрации в url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import qs from 'qs';
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import { SearchContext } from '../App';
 
 import Categories from '../components/Categories';
-import Sort from '../components/Sort';
+import Sort, { sortList } from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Pagination from '../components/Pagination';
-import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
+import { setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
 
 export const Home = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const isSearch = React.useRef(false);
+  const isMounted = React.useRef(false);
 
   // const categoryId = useSelector((state) => state.filter.categoryId);
   const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
@@ -32,7 +37,7 @@ export const Home = () => {
     dispatch(setCurrentPage(number));
   };
 
-  React.useEffect(() => {
+  const fetchPizzas = () => {
     setIsLoading(true);
 
     const sortBy = sortType.replace('-', '');
@@ -48,8 +53,56 @@ export const Home = () => {
         setItems(response.data);
         setIsLoading(false);
       });
+  };
 
+  React.useEffect(() => {
+    // Если изменили параметры и уже был первый рендер
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        sortProperty: sort.sortProperty,
+        categoryId,
+        currentPage,
+      });
+
+      navigate(`?${queryString}`);
+
+      console.log('3 useEffect', queryString);
+    }
+    isMounted.current = true; // первый рендер был
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  // Первый рендер + строка запроса, то проверяем юрл-параметры и сохраняем в редакс
+  React.useEffect(() => {
+    console.log('1.0 useEffect');
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+
+      // нахождение объекта
+      const sort = sortList.find((obj) => obj.sortProperty === params.sortProperty);
+
+      dispatch(
+        setFilters({
+          ...params,
+          sort,
+        }),
+      );
+      console.log('1.1 useEffect');
+      isSearch.current = true; // чтобы fetchPizzas не вызывался 2 раза при первом рендере
+    }
+  }, []);
+
+  React.useEffect(() => {
+    console.log('2.0 useEffect');
     window.scrollTo(0, 0);
+
+    if (!isSearch.current) {
+      console.log('2.05 useEffect');
+      fetchPizzas();
+    }
+
+    isSearch.current = false;
+
+    console.log('2.1 useEffect');
   }, [categoryId, sortType, searchValue, currentPage]);
 
   const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
